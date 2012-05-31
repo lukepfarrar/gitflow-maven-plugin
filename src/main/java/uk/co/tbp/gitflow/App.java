d9045e14beb1e7e001c2b152fdfaac852e5a577f
Luke Farrar <luke.farrar@thebookpeople.co.uk>
2012-05-31 18:03:18 +0100

    initial commit

---
+++ src/main/java/uk/co/tbp/gitflow/App.java
@@ -0,0 +1,13 @@
+package uk.co.tbp.gitflow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
